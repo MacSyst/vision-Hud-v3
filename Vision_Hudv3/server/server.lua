668aa81dda69624b6a324aa27e2c68204068d11e
Vision-Scripts <167629435+MacSyst@users.noreply.github.com>
2024-05-14 18:06:00 +0200

    Update server.lua

--- Vision_Hudv3/server/server.lua
+++ Vision_Hudv3/server/server.lua
@@ -1,8 +1,5 @@
 -- Made By Benni (Vision-Service) with <3 for the Community.
 
-
-local xPlayer = ESX.GetPlayerFromId(source)
-
 ESX.RegisterServerCallback('vision_hudv2:getaccounts', function(src, cb)
 	local xPlayer = ESX.GetPlayerFromId(src)
   local accounts = {
